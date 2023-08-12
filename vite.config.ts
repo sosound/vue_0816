b5dd4bb4853a315a334071999ad612e569ca6e42
叫我汉高祖 <793032750@qq.com>
2023-08-12 13:49:45 +0800

    解决跨域

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,32 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
-    host: '0.0.0.0'
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    // 配置代理
+    proxy: {
+      // 请求的路径前缀只要是 /api 就会被拦截走这个代理
+      '/api': {
+        /**
+         *  请求的目标资源再经过替换成 /httphwm/getList 后，
+         *  会加上 http://127.0.0.1:9693 这个前缀，
+         *  最后请求的URL为: http://127.0.0.1:9693/httphwm/getList
+         */
+        target: 'http://127.0.0.1:9693',
+        ws: true,
+        secure: true,
+        changeOrigin: true,
+        // 拦截到的请求路径 api/httphwm/getList，/api会被替换成空
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
